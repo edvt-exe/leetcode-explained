14963b66c1c2ffc53d6626641f63b7ddc97721c6
Voicu Eduard-Alexandru <eduardvoicu05@gmail.com>
2026-09-20 17:50:20 +0300

    Time: 0 ms (100%), Space: 19.5 MB (36.71%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        result = []
+
+        def backtrack(current, opened, closed):
+            if len(current) == 2 * n:
+                result.append(current)
+                return
+
+            if opened < n:
+                backtrack(current + "(", opened + 1, closed)
+
+            if closed < opened:
+                backtrack(current + ")", opened, closed + 1)
+
+        backtrack("", 0, 0)
+
+        return result
